001caba04b36d889550dc50d00871e287e3b13cc
RoxanaPascu <p.roxana2408@gmail.com>
2021-09-25 19:59:29 +0300

    Week 04 - VTA Calculator

--- src/main/java/ro/sci/VTA_Calculator/VTA.java
+++ src/main/java/ro/sci/VTA_Calculator/VTA.java
@@ -8,7 +8,7 @@ public class VTA {
     public static void main(String[] args) {
 
         Scanner scanner = new Scanner(System.in);
-        System.out.println("Please introduce the price of the product without VTA: ");
+        System.out.println("Please insert the price of the product without VTA: ");
         double priceWithoutVTA = scanner.useLocale(Locale.ENGLISH).nextDouble();
 
         if (priceWithoutVTA > 0) {
@@ -20,7 +20,7 @@ public class VTA {
 
     private static double calculatePrice(double priceWithoutVTA) {
         Scanner scanner = new Scanner(System.in);
-        System.out.println("Please introduce the VTA: ");
+        System.out.println("Please insert the VTA: ");
         double VTA = scanner.useLocale(Locale.ENGLISH).nextDouble();
         double priceWithVTA = priceWithoutVTA + VTA / 100 * priceWithoutVTA;
         return priceWithVTA;
