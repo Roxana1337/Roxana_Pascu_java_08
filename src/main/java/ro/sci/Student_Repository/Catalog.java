3191f72d7625442580cfecf12f067a0bd295fedc
RoxanaPascu <p.roxana2408@gmail.com>
2021-11-01 22:10:54 +0200

    Week 07 - Exceptions and logging

--- src/main/java/ro/sci/Student_Repository/Catalog.java
+++ src/main/java/ro/sci/Student_Repository/Catalog.java
@@ -1,56 +1,95 @@
 package ro.sci.Student_Repository;
 
-import sun.java2d.pipe.SpanShapeRenderer;
 
-import java.text.SimpleDateFormat;
+import java.time.LocalDate;
+import java.time.Period;
+import java.time.format.DateTimeFormatter;
 import java.util.*;
 
 public class Catalog {
 
     HashMap<String, Student> studentCatalog = new HashMap<>();
-    TreeMap<String, Student> studentCatalogByLastName = new TreeMap<>();
-    HashMap<String, Student> studentCatalogByBirthDate = new HashMap<>();
 
     public void addStudent(String firstName, String lastName, String dateOfBirth, String gender, String cnp) {
-        Student student = new Student(firstName, lastName, dateOfBirth, gender, cnp);
+
         if (firstName == null || lastName == null || firstName.trim().isEmpty() || lastName.trim().isEmpty()) {
-            throw new IllegalArgumentException ("First and last name are mandatory");
+            throw new IllegalArgumentException("First and last name are mandatory");
         }
         if (!Objects.equals(gender, "M") && !Objects.equals(gender, "F") && !Objects.equals(gender, "m")
                 && !Objects.equals(gender, "f")) {
             throw new IllegalArgumentException("Gender must be m or f (case insensitive)");
         }
-        studentCatalog.put(cnp, student);
-        studentCatalogByLastName.put(lastName, student);
-        studentCatalogByBirthDate.put(dateOfBirth, student);
+        DateTimeFormatter sourceFormat = DateTimeFormatter.ofPattern("dd/MM/yyyy");
+        LocalDate birthDate = LocalDate.parse(dateOfBirth, sourceFormat);
+
+        if (birthDate.getYear() < 1900 || birthDate.getYear() > LocalDate.now().minusYears(18).getYear()) {
+            throw new IllegalArgumentException("Birth date must be valid between 1900 and current year-18");
+        }
+        studentCatalog.put(cnp, new Student(firstName, lastName, dateOfBirth, gender, cnp));
     }
 
     public void deleteStudentByCNP(String cnp) {
-            if (studentCatalog.containsKey(cnp)) {
-                studentCatalog.remove(cnp);
-                studentCatalogByLastName.remove(cnp);
-                studentCatalogByBirthDate.remove(cnp);
-            } else
-                throw new NoSuchElementException("ID number not valid");
-        }
+        if (studentCatalog.containsKey(cnp)) {
+            studentCatalog.remove(cnp);
+        } else
+            throw new NoSuchElementException("ID number not valid");
+    }
+
+    //TODO TreeMap iterare
 
     public void listCatalog() {
-        if(studentCatalog.isEmpty()) {
+        if (studentCatalog.isEmpty()) {
             throw new NoSuchElementException("No students to display");
         }
         System.out.println(Arrays.asList(studentCatalog));
     }
 
     public void listCatalogByLastName() {
-        if (studentCatalogByLastName.isEmpty()) {
+        if (studentCatalog.isEmpty()) {
             throw new NoSuchElementException("No students to display");
         }
+        TreeMap<String, Student> studentCatalogByLastName = new TreeMap<>();
+        Set<String> cnpSet = studentCatalog.keySet();
+        for (String cnp : cnpSet) {
+            Student student = studentCatalog.get(cnp);
+            studentCatalogByLastName.put(student.getLastName(), student);
+        }
+
         System.out.println(Arrays.asList(studentCatalogByLastName));
     }
+    //TODO birthdate si list by age X
 
+    public void listCatalogByAge() {
+        TreeMap<LocalDate, Student> studentCatalogByAge = new TreeMap<>();
+        Set<String> cnpSet = studentCatalog.keySet();
+        DateTimeFormatter sourceFormat = DateTimeFormatter.ofPattern("dd/MM/yyyy");
+        for (String cnp : cnpSet) {
+            LocalDate dateOfBirth = LocalDate.parse(studentCatalog.get(cnp).getDateOfBirth(), sourceFormat);
+            Student student = studentCatalog.get(cnp);
+            studentCatalogByAge.put(dateOfBirth, student);
+        }
+        System.out.println(Arrays.asList(studentCatalogByAge));
+    }
 
-//    public void dateOfBirthNumber() {
-//        int studentBirthDay = Integer.parseInt(Student.getDateOfBirth
-//
-//    }
+    public void retrieveStudentsByAge(String ageFromDOB) {
+        try {
+            int age = Integer.parseInt(ageFromDOB);
+            ArrayList<Student> studentByAge = new ArrayList<>();
+            Set<String> cnpSet = studentCatalog.keySet();
+            DateTimeFormatter sourceFormat = DateTimeFormatter.ofPattern("dd/MM/yyyy");
+            for(String cnp : cnpSet) {
+                LocalDate dateOfBirth = LocalDate.parse(studentCatalog.get(cnp).getDateOfBirth(), sourceFormat);
+                int ageOfStudent = Period.between(dateOfBirth, LocalDate.now()).getYears();
+                if(ageOfStudent == age) {
+                    studentByAge.add(studentCatalog.get(cnp));
+                }
+            }
+            System.out.println(studentByAge);
+        }
+        catch (NumberFormatException exception) {
+            throw new NumberFormatException("Age must be a number");
+        }
+
+    }
+
 }
--- src/main/java/ro/sci/Student_Repository/Main.java
+++ src/main/java/ro/sci/Student_Repository/Main.java
@@ -8,12 +8,17 @@ public class Main {
         Catalog studentCatalogByLastName = new Catalog();
         studentCatalog.addStudent("Stefana", "Panaite", "18/11/1997", "f", "2971118226718");
         studentCatalog.addStudent("Vasile", "Ionescu", "12/06/2001", "m", "1010612224712");
+        studentCatalog.addStudent("Ovidiu", "Avasilcai", "01/11/2020", "f", "2201101226718");
         studentCatalog.addStudent("Mariana", "Andries", "07/02/2000", "M", "2000207221165");
         studentCatalog.listCatalog();
 //        studentCatalog.deleteStudentByCNP("297111822678");
         studentCatalog.deleteStudentByCNP("2971118226718");
         studentCatalog.listCatalog();
         studentCatalog.listCatalogByLastName();
+        studentCatalog.listCatalogByAge();
+        studentCatalog.retrieveStudentsByAge("20");
+//        studentCatalog.addStudent("Vasile", "PreaBatran", "12/06/1879", "m", "3790612224712");
+
 
 
     }
